4a15c7caf68a73917a1430ecab125047390e07d4
WanjohiK <kelvinnyawira2022@gmail.com>
2024-01-05 23:44:15 +0300

    python

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+101-locked_class Module
+"""
+
+
+class LockedClass:
+    """
+    define a locked class
+    """
+    #  define the available slot that can be created
+    __slots__ = ('first_name')
