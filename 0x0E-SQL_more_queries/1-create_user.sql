83be3930bdcf5f288a15e2bb6a6641b3959f14b0
WanjohiK <kelvinnyawira2022@gmail.com>
2024-02-14 14:32:27 +0300

    python

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,12 @@
+-- script that creates the MySQL server user user_0d_1.
+
+-- user_0d_1 should have all privileges on your MySQL server
+-- The user_0d_1 password should be set to user_0d_1_pwd
+-- If the user user_0d_1 already exists, your script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- running the FLUSH PRIVILEGES command in order to reload the grant tables to ensure that the new privileges are put into effect:
+FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,16 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+-- If the database hbtn_0d_2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
+-- creating the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- creating user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
+
+-- granting permissions to user_0d_2 for the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
